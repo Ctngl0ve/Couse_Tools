07307a693f6022b208afaf05e12d8ccc39ca3c5b
user <user@WIN-RCHCIG90JOE>
2025-02-09 00:33:34 +0300

    Добавьте файлы проекта.

---
+++ Задание1_1/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Задание1_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Погнали");
+            Console.ReadLine();
+        }
+    }
+}
